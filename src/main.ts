1cbd707fd1a7310c17f305b56c2ee9a4cc323c4d
GaganSharma <gagan3103sharma2000@gmail.com>
2021-06-03 13:27:48 +0530

    models

--- src/main.ts
+++ src/main.ts
@@ -50,7 +50,7 @@ const allSkills:Skill[]=[
 	},
 	
 	{
-	image:'assests/my.jpg',
+	image:'assets/my.jpg',
 	title:'CSS',
 	level:5,
 	},
@@ -80,7 +80,7 @@ faceCube.position.set(-3,1.5,12)
 scene.add(faceCube);
 
 //init
-(()=>{
+(async()=>{
 
 	//skill section elemments
 	const skillSec=document.querySelector('#skillSection')!
@@ -106,6 +106,7 @@ scene.add(faceCube);
 
 	// Lights
 	const ambientLight = new THREE.AmbientLight(0xffffff)
+	ambientLight.intensity=0.5
 	scene.add( ambientLight)
 
 	//floor	
@@ -128,7 +129,10 @@ scene.add(faceCube);
 	})
 	)
 	meshFloor.rotation.x -= Math.PI / 2 // Rotate the floor 90 degrees
+	//TODO
 	scene.add(meshFloor)	
+	const gtloader = new GLTFLoader()
+
 //============== objects
 	//** stars**
 	 //star
@@ -144,42 +148,212 @@ scene.add(faceCube);
 		star.position.set(x, y, z)
 		scene.add(star)
 	  }
-	  
-	for(let i=0;i<500;i++)addStar()
-	  
+	//===============assets
+	//fall tree
+	gtloader.load('assets/models/falltree/model.gltf',
+	(gltfmodel)=>{
+	const tree= gltfmodel.scene
+	
+	tree.scale.setScalar(1)
+	tree.position.set(0,3,22)
+	//g(tree)
+	tree.children[2].visible=false
+	let i=0
+	while(i<6){
+		addrandomonfloor(tree.clone(),3)
+		i++
+	}
+	})//bldr
+	gtloader.load('assets/models/bouldar/boulder_03.gltf',
+	(gltfmodel)=>{
+	const bldr= gltfmodel.scene
+	bldr.scale.setScalar(0.01)
+	bldr.position.set(0,0,22)
+	bldr.children[2].visible=false
+	//g('bldr')
+	//g(bldr)
+	bldr.rotation.y+=Math.PI/2
+	bldr.children[2].visible=false
+	let i=0
+	while(i<3){
+		addrandomonfloor(bldr.clone(),0)
+		i++
+	}
+	})
+	//cactus
+	gtloader.load('assets/models/cactus/PipeOrganCactus.gltf',
+	(gltfmodel)=>{
+	const cactus= gltfmodel.scene
+	cactus.scale.setScalar(0.3)
+	cactus.position.set(0,0,22)
+	cactus.children[2].visible=false
+	//g('bldr')
+	//g(bldr)
+	let i=0
+	while(i<7){
+		addrandomonfloor(cactus.clone(),0)
+		i++
+	}
+	})
+	//been
+	gtloader.load('assets/models/beanstalk/PUSHILIN_Beanstalk.gltf',
+	(gltfmodel)=>{
+	const bean= gltfmodel.scene
+	bean.scale.setScalar(1)
+	bean.position.set(0,0,22)
+	bean.children[2].visible=false
+	//g('bldr')
+	//g(bldr)
+	let i=0
+	while(i<7){
+		addrandomonfloor(bean.clone(),0)
+		i++
+	}
+	})
+	//babmm
+	gtloader.load('assets/models/bamboo/PUSHILIN_bamboo.gltf',
+	(gltfmodel)=>{
+	const bamboo= gltfmodel.scene
+//	bamboo.scale.setScalar(0.3)
+	bamboo.children[2].visible=false
+	//g('bldr')
+	//g(bldr)
+	let i=0
+
+	while(i<7){
+		addrandomonfloor(bamboo.clone(),1)
+		i++
+	}
+	})
+	//willow
+	gtloader.load('assets/models/weepwillow/WeepingWillowTree.gltf',
+	(gltfmodel)=>{
+	const tree= gltfmodel.scene.children[0]
+	tree.scale.setScalar(0.5)
+	//g(tree)
+	 let i=0
+	 while(i<6){
+	 	addrandomonfloor(tree.clone())
+	 	i++
+	 }
+	})
+
+	function addrandomonfloor(obj:THREE.Object3D,yoffset?:number) {
+		const [x, z] =[
+		THREE.MathUtils.randFloat(-30,30),
+		THREE.MathUtils.randFloat(0,200)
+		]
+		obj.position.set(x, yoffset||0, z)
+		scene.add(obj)
+	  }
+  
+	let znow=16;  
+	for(let i=0;i<500;i++)addStar(); 
 	mesh.position.y += 3
 	mesh.position.z=2
    // Move the mesh up 1 meter
 	scene.add(mesh)
 	//skill board 3d model
-	const gtloader = new GLTFLoader()
-	gtloader.load( 'assets/models/board.json', function ( gltf ) {
+	await gtloader.load( 'assets/models/construction_barricades/scene.gltf', function ( gltfmodel ) {
+		const gltf=gltfmodel.scene
+		//.children[0].children[0]
+		//g('signs loaded')
+		const standboard=gltf.clone()
+		const plankboard=gltf.clone()
+		console.log('st', standboard,'pl',plankboard)
+	for(let i=0;i<standboard.children[0].children[0].children[0].children.length;i++){
+		standboard.children[0].children[0].children[0].children[i].visible=i<4
+		plankboard.children[0].children[0].children[0].children[i].visible=!(i<4)
+		if(i>10){
+			plankboard.children[0].children[0].children[0].children[i].visible=false
+		}
+	}
+	standboard.position.set(1.5,1,15)
+	standboard.rotation.y+=Math.PI
+	scene.add(standboard)
 		for(let i=0;i<allSkills.length;i++){
-		   const geometry = new THREE.PlaneGeometry(0.5,0.5)
+		   const geometry = new THREE.PlaneGeometry(0.3,0.3)
 		   const material = new THREE.MeshStandardMaterial({
 			   map:new THREE.TextureLoader().load(allSkills[i].image),
 			   transparent:true
 			 //  displacementScale:-1
 			 })
 		   const hoard = new THREE.Mesh(geometry, material)
-		   const skillBoard = gltf.scene.clone()
-		   hoard.position.y=1.5
-		   hoard.position.z-=0.25
-		   hoard.position.x+=0.4
+		   const skillBoard = plankboard.clone()
+		   hoard.position.set(0.6,-0.2,0.05)
 		   skillBoard.add(hoard)
-		   skillBoard.castShadow=true
-		   skillBoard.position.set(1.5,0,17+i*2)
+		   ////g(allSkills[i])
+		   znow+= 1.5
+		   skillBoard.position.set(1.5,1,znow)
 		   skillBoard.rotation.y+=Math.PI
-		   {
-			   skillBoard.children[1].visible=false
-			   skillBoard.children[2].visible=false
-		   }
-		   console.log(skillBoard.children)
 		   scene.add(skillBoard)
 		 }
-		   }, undefined, function ( error ) {
-			   console.error( error )
-		   } )	   
+		 for(let i=0;i<tool.length;i++){
+			const geometry = new THREE.PlaneGeometry(0.3,0.3)
+			const material = new THREE.MeshStandardMaterial({
+				map:new THREE.TextureLoader().load(tool[i].image),
+				transparent:true
+			  //  displacementScale:-1
+			  })
+			const hoard = new THREE.Mesh(geometry, material)
+			const skillBoard = plankboard.clone()
+			hoard.position.set(0.6,-0.2,0.05)
+			skillBoard.add(hoard)
+			znow+= 1.5
+			skillBoard.position.set(1.5,1,znow)
+			skillBoard.rotation.y+=Math.PI
+			scene.add(skillBoard)
+		  }
+	}, undefined, function ( error ) {
+	console.error( error )
+	} )
+	gtloader.load('assets/models/buld/Bulldozer.gltf',
+	(gltfmodel)=>{
+	const bulldozer= gltfmodel.scene
+	
+	bulldozer.scale.setScalar(0.3)
+	bulldozer.position.set(-4,0.1,22)
+	//g(bulldozer)
+	bulldozer.children[2].visible=false
+	bulldozer.rotateY(3*Math.PI/4)
+	scene.add(bulldozer)
+	})
+	//truck
+	gtloader.load('assets/models/dumbtruck/Dump_Truck_01.gltf',
+	(gltfmodel)=>{
+	const truck= gltfmodel.scene
+	truck.children[2].visible=false
+	truck.scale.setScalar(0.3)
+	//g('scaffold')
+	truck.position.set(-4,0,30)
+	truck.rotateY(3*Math.PI/4)
+	scene.add(truck)
+	},undefined,console.log)
+	// constr house
+	gtloader.load('assets/models/cabin/Cabin 2.gltf',
+	(gltfmodel)=>{
+	const house= gltfmodel.scene
+	house.scale.setScalar(0.01)
+	//g('scaffold00')
+	house.children[2].visible=false
+
+	house.position.set(-15,0,40)
+	house.rotateY(3*Math.PI/4)
+	scene.add(house)
+	},undefined,console.log)
+	//wood
+	gtloader.load('assets/models/woodpile/model.gltf',
+	(gltfmodel)=>{
+	const wpile= gltfmodel.scene
+	wpile.scale.setScalar(4)
+	//g('plants')
+	//g(gltfmodel)
+	console.log('wp',wpile)
+
+	wpile.position.set(7,-5,30)
+	wpile.rotateY(3*Math.PI/4)
+	scene.add(wpile)
+	},undefined,console.log)
 })()
 
 function keyDown(event:KeyboardEvent){
